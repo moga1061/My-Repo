e15e33ae153647c7115841ce4727fc34538990b9
theyid <tcz009@hotmail.co.uk>
2013-07-14 18:17:56 +0100

    4U

---
+++ plugin.video.filmlinks4u/default.py
@@ -0,0 +1,246 @@
+import xbmc, xbmcgui, xbmcaddon, xbmcplugin
+import urllib, urllib2
+import re, string, sys, os
+import urlresolver
+from TheYid.common.addon import Addon
+from TheYid.common.net import Net
+from htmlentitydefs import name2codepoint as n2cp
+import HTMLParser
+
+try:
+	from sqlite3 import dbapi2 as sqlite
+	print "Loading sqlite3 as DB engine"
+except:
+	from pysqlite2 import dbapi2 as sqlite
+	print "Loading pysqlite2 as DB engine"
+
+addon_id = 'plugin.video.filmlinks4u'
+plugin = xbmcaddon.Addon(id=addon_id)
+
+DB = os.path.join(xbmc.translatePath("special://database"), 'filmlinks4u.db')
+BASE_URL = 'http://www.filmlinks4u.net/'
+net = Net()
+addon = Addon('plugin.video.filmlinks4u', sys.argv)
+showAllParts = True
+showPlayAll = True
+
+#PATHS
+AddonPath = addon.get_path()
+IconPath = AddonPath + "/icons/"
+
+if plugin.getSetting('showAllParts') == 'false':
+        showAllParts = False
+
+if plugin.getSetting('showPlayAll') == 'false':
+        showPlayAll = False
+
+##### Queries ##########
+mode = addon.queries['mode']
+url = addon.queries.get('url', None)
+content = addon.queries.get('content', None)
+query = addon.queries.get('query', None)
+startPage = addon.queries.get('startPage', None)
+numOfPages = addon.queries.get('numOfPages', None)
+listitem = addon.queries.get('listitem', None)
+urlList = addon.queries.get('urlList', None)
+section = addon.queries.get('section', None)
+##### Queries ##########
+
+
+def GetTitles(section, url, startPage= '1', numOfPages= '1'): # Get Movie Titles
+        print 'filmlinks4u get Movie Titles Menu %s' % url
+
+        # handle paging
+        pageUrl = url
+        if int(startPage)> 1:
+                pageUrl = url + 'page/' + startPage + '/'
+        print pageUrl
+        html = net.http_GET(pageUrl).content
+        CLEAN(html)
+
+        start = int(startPage)
+        end = start + int(numOfPages)
+
+        for page in range( start, end):
+                if ( page != start):
+                        pageUrl = url + 'page/' + str(page) + '/'
+                        html = net.http_GET(pageUrl).content
+                        CLEAN(html)
+                        
+                match = re.compile('<h2 class=.+?href="(.+?)".+?>(.+?)<.+?', re.DOTALL).findall(html)
+                for movieUrl, name in match:
+                        cm  = []
+                        runstring = 'XBMC.Container.Update(plugin://plugin.video.filmlinks4u/?mode=Search&query=%s)' %(name.strip())
+        		cm.append(('Search on filmlinks4u', runstring))
+                        addon.add_directory({'mode': 'GetLinks', 'section': section, 'url': movieUrl}, {'title':  name.strip()}, contextmenu_items= cm)
+
+
+
+                addon.add_directory({'mode': 'GetTitles', 'url': url, 'startPage': str(end), 'numOfPages': numOfPages}, {'title': '[COLOR blue][B][I]Next page...[/B][/I][/COLOR]'}, img=IconPath + 'icon.png')
+        
+       	xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+
+def GetLinks(section, url): # Get Links
+        print 'GETLINKS FROM URL: '+url
+        html = net.http_GET(str(url)).content
+        CLEAN(html)
+        sources = []
+        listitem = GetMediaInfo(html)
+        print 'LISTITEM: '+str(listitem)
+        content = html
+        print'CONTENT: '+str(listitem)
+        r = re.search('<strong>Links.*</strong>', html)
+        if r:
+                content = html[r.end():]
+                
+        r = re.search('commentblock', content)
+        if r:
+                content = content[:r.start()]
+
+        match = re.compile('href="(.+?)"').findall(content)
+        listitem = GetMediaInfo(content)
+        for url in match:
+                host = GetDomain(url)
+
+                if 'Unknown' in host:
+                                continue
+
+                if r:
+                        continue
+                print '*****************************' + host
+                title = url.rpartition('/')
+                host = host.replace('movreel.com','movreel.com - [COLOR red]Download/Streaming limit of 2GB a day !![/COLOR]')
+                name = host
+                hosted_media = urlresolver.HostedMediaFile(url=url, title=name)
+                sources.append(hosted_media)
+
+        source = urlresolver.choose_source(sources)
+        if source: stream_url = source.resolve()
+        else: stream_url = ''
+        xbmc.Player().play(stream_url)
+
+                       
+
+def CLEAN(string):
+    def substitute_entity(match):
+        ent = match.group(3)
+        if match.group(1) == "#":
+            if match.group(2) == '':
+                return unichr(int(ent))
+            elif match.group(2) == 'x':
+                return unichr(int('0x'+ent, 16))
+        else:
+            cp = n2cp.get(ent)
+            if cp: return unichr(cp)
+            else: return match.group()
+    entity_re = re.compile(r'&(#?)(x?)(\d{1,5}|\w{1,8});')
+    return entity_re.subn(substitute_entity, string)[0]
+
+
+def GetDomain(url):
+        tmp = re.compile('//(.+?)/').findall(url)
+        domain = 'Unknown'
+        if len(tmp) > 0 :
+            domain = tmp[0].replace('www.', '')
+        return domain
+
+
+def GetMediaInfo(html):
+        listitem = xbmcgui.ListItem()
+        match = re.search('og:title" content="(.+?) \((.+?)\)', html)
+        if match:
+                print match.group(1) + ' : '  + match.group(2)
+                listitem.setInfo('video', {'Title': match.group(1), 'Year': int(match.group(2)) } )
+        return listitem
+
+def Categories(section):  #categories
+
+        html = net.http_GET(BASE_URL).content
+        addon.add_directory({'mode': 'GetTitles', 'section': section, 'url': BASE_URL + '/category/' + section,
+                             'startPage': '1', 'numOfPages': '2'}, {'title':  'All'})
+
+        match = re.compile('<li class="cat-item.+?/category/' + section + '/(.+?)".+?>(.+?)<').findall(html)
+        for cat, title in match:
+                url = BASE_URL + '/category/' + section + '/' + cat
+                addon.add_directory({'mode': 'GetTitles', 'section': section, 'url': url,
+                                     'startPage': '1', 'numOfPages': '2'}, {'title':  title})
+        
+        xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+def MainMenu():    #homescreen
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/hollywood',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  '[COLOR blue]Hollywood >>[/COLOR]'}, img=IconPath + 'icon.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/dubbed',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Dubbed >>'}, img=IconPath + 'icon.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/hindi',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Hindi >>'}, img=IconPath + 'icon.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/telugu',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Telugu >>'}, img=IconPath + 'icon.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/tamil',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Tamil >>'}, img=IconPath + 'icon.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/gujarati',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Gujarati >>'}, img=IconPath + 'icon.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/malayalam',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Malayalam >>'}, img=IconPath + 'icon.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/kannada',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Kannada >>'}, img=IconPath + 'icon.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/punjabi',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Punjabi >>'}, img=IconPath + 'icon.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/bengali',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Bengali >>'}, img=IconPath + 'icon.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/malayalam',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Malayalam >>'}, img=IconPath + 'icon.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/animation',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  '[COLOR blue]Animation >>[/COLOR]'}, img=IconPath + 'icon.png')
+        addon.add_directory({'mode': 'GetSearchQuery'},  {'title':  '[COLOR green]Search[/COLOR]'}, img=IconPath + 'icon.png')
+        addon.add_directory({'mode': 'ResolverSettings'}, {'title':  '[COLOR red]Resolver Settings[/COLOR]'}, img=IconPath + 'icon.png')
+        addon.add_directory({'mode': 'Help'}, {'title':  '[COLOR pink]FOR HELP ON THIS ADDON PLEASE GOTO...[/COLOR] [COLOR gold][B][I]www.xbmchub.com[/B][/I][/COLOR]'}, img=IconPath + 'icon.png')
+        xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+
+
+def GetSearchQuery():
+	last_search = addon.load_data('search')
+	if not last_search: last_search = ''
+	keyboard = xbmc.Keyboard()
+        keyboard.setHeading('[COLOR green]Search filmlinks4u[/COLOR]')
+	keyboard.setDefault(last_search)
+	keyboard.doModal()
+	if (keyboard.isConfirmed()):
+                query = keyboard.getText()
+                addon.save_data('search',query)
+                Search(query)
+	else:
+                return
+
+        
+def Search(query):
+        url = 'http://www.google.com/search?q=site:filmlinks4u.net ' + query
+        url = url.replace(' ', '+')
+        print url
+        html = net.http_GET(url).content
+        CLEAN(html)
+        match = re.compile('<h3 class="r"><a href="(.+?)".+?onmousedown=".+?">(.+?)</a>').findall(html)
+        for url, title in match:
+                title = title.replace('<b>...</b>', '').replace('<em>', '').replace('</em>', '')
+                addon.add_directory({'mode': 'GetLinks', 'url': url}, {'title':  title})
+	xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+
+if mode == 'main': 
+	MainMenu()
+elif mode == 'GetTitles': 
+	GetTitles(section, url, startPage, numOfPages)
+elif mode == 'GetLinks':
+	GetLinks(section, url)
+elif mode == 'GetSearchQuery':
+	GetSearchQuery()
+elif mode == 'Search':
+	Search(query)
+elif mode == 'PlayVideo':
+	PlayVideo(url, listitem)	
+elif mode == 'ResolverSettings':
+        urlresolver.display_settings()
+elif mode == 'Categories':
+        Categories(section)
