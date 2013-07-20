287ad3410c73ebe654757314b48082ff4fc0ee2a
theyid <tcz009@hotmail.co.uk>
2013-07-20 22:39:25 +0100

    cine vip

---
+++ plugin.video.cinevip/default.py
@@ -0,0 +1,261 @@
+import xbmc, xbmcgui, xbmcaddon, xbmcplugin
+import urllib, urllib2
+import re, string, sys, os
+import urlresolver
+from TheYid.common.addon import Addon
+from TheYid.common.net import Net
+from htmlentitydefs import name2codepoint as n2cp
+import HTMLParser
+
+try:
+	from sqlite3 import dbapi2 as sqlite
+	print "Loading sqlite3 as DB engine"
+except:
+	from pysqlite2 import dbapi2 as sqlite
+	print "Loading pysqlite2 as DB engine"
+
+addon_id = 'plugin.video.cinevip'
+plugin = xbmcaddon.Addon(id=addon_id)
+
+DB = os.path.join(xbmc.translatePath("special://database"), 'cinevip.db')
+BASE_URL = 'http://www.cinevip.org/'
+net = Net()
+addon = Addon('plugin.video.cinevip', sys.argv)
+showAllParts = True
+showPlayAll = True
+
+#PATHS
+AddonPath = addon.get_path()
+IconPath = AddonPath + "/icons/"
+
+if plugin.getSetting('showAllParts') == 'false':
+        showAllParts = False
+
+if plugin.getSetting('showPlayAll') == 'false':
+        showPlayAll = False
+
+##### Queries ##########
+mode = addon.queries['mode']
+url = addon.queries.get('url', None)
+content = addon.queries.get('content', None)
+query = addon.queries.get('query', None)
+startPage = addon.queries.get('startPage', None)
+numOfPages = addon.queries.get('numOfPages', None)
+listitem = addon.queries.get('listitem', None)
+urlList = addon.queries.get('urlList', None)
+section = addon.queries.get('section', None)
+##### Queries ##########
+
+
+def GetTitles(section, url, startPage= '1', numOfPages= '1'): # Get Movie Titles
+        print 'cinevip get Movie Titles Menu %s' % url
+
+        # handle paging
+        pageUrl = url
+        if int(startPage)> 1:
+                pageUrl = url + 'page/' + startPage + '/'
+        print pageUrl
+        html = net.http_GET(pageUrl).content
+        CLEAN(html)
+
+        start = int(startPage)
+        end = start + int(numOfPages)
+
+        for page in range( start, end):
+                if ( page != start):
+                        pageUrl = url + 'page/' + startPage + '/'
+                        html = net.http_GET(pageUrl).content
+                        CLEAN(html)
+                        
+                match = re.compile('entry-cdvd.+?href="(.+?)"><img alt=(.+?)".+?src="(.+?)".+?', re.DOTALL).findall(html)
+                for movieUrl, name, img in match:
+                        cm  = []
+                        runstring = 'XBMC.Container.Update(plugin://plugin.video.cinevip/?mode=Search&query=%s)' %(name.strip())
+        		cm.append(('Search on cinevip', runstring))
+                        addon.add_directory({'mode': 'GetLinks', 'section': section, 'url': movieUrl}, {'title':  name.strip()}, contextmenu_items= cm, img= img)
+                
+
+      
+                addon.add_directory({'mode': 'GetTitles', 'url': url, 'startPage': str(end), 'numOfPages': numOfPages}, {'title': '[COLOR red][B][I]No Next Page Yet are work on it soon[/B][/I][/COLOR]'}, img=IconPath + '1.png')
+        
+       	xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+
+def GetLinks(section, url): # Get Links
+        print 'GETLINKS FROM URL: '+url
+        html = net.http_GET(str(url)).content
+        CLEAN(html)
+        sources = []
+        listitem = GetMediaInfo(html)
+        print 'LISTITEM: '+str(listitem)
+        content = html
+        print'CONTENT: '+str(listitem)
+        r = re.search('<strong>Links.*</strong>', html)
+        if r:
+                content = html[r.end():]
+                
+        r = re.search('commentblock', content)
+        if r:
+                content = content[:r.start()]
+
+        match = re.compile('href="(.+?)"').findall(content)
+        listitem = GetMediaInfo(content)
+        for url in match:
+                host = GetDomain(url)
+
+                if 'Unknown' in host:
+                                continue
+
+                if r:
+                        continue
+                print '*****************************' + host
+                title = url.rpartition('/')
+                name = host
+                hosted_media = urlresolver.HostedMediaFile(url=url, title=name)
+                sources.append(hosted_media)
+
+        source = urlresolver.choose_source(sources)
+        if source: stream_url = source.resolve()
+        else: stream_url = ''
+        xbmc.Player().play(stream_url)
+
+                       
+
+def CLEAN(string):
+    def substitute_entity(match):
+        ent = match.group(3)
+        if match.group(1) == "#":
+            if match.group(2) == '':
+                return unichr(int(ent))
+            elif match.group(2) == 'x':
+                return unichr(int('0x'+ent, 16))
+        else:
+            cp = n2cp.get(ent)
+            if cp: return unichr(cp)
+            else: return match.group()
+    entity_re = re.compile(r'&(#?)(x?)(\d{1,5}|\w{1,8});')
+    return entity_re.subn(substitute_entity, string)[0]
+
+
+def GetDomain(url):
+        tmp = re.compile('//(.+?)/').findall(url)
+        domain = 'Unknown'
+        if len(tmp) > 0 :
+            domain = tmp[0].replace('www.', '')
+        return domain
+
+
+def GetMediaInfo(html):
+        listitem = xbmcgui.ListItem()
+        match = re.search('og:title" content="(.+?) \((.+?)\)', html)
+        if match:
+                print match.group(1) + ' : '  + match.group(2)
+                listitem.setInfo('video', {'Title': match.group(1), 'Year': int(match.group(2)) } )
+        return listitem
+
+
+def MainMenu():    #homescreen
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/accion',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Accion >>'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/animacion',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Animacion >>'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/aventura',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Aventura >>'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + 'category/biografia',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Biografia >>'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/castellano',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Castellano >>'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/cine',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Cine >>'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/comedia',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Comedia >>'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/crimen',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Crimen >>'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/deportes',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Deportes >>'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/documental',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Documental >>'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/drama',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Drama >>'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/dvdrip',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'DVDRip >>'}, img=IconPath + '1.png')
+
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/familiar',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Familiar >>'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/featured',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Featured >>'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/guerra',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Guerra >>'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/infantil',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Infantil >>'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/latino',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Latino >>'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/misterio',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Misterio >>'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/romance',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Romance >>'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/sci-fi',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Sci-fi >>'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/subesp',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'SubEsp >>'}, img=IconPath + '1.png')
+
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/suspenso',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Suspenso >>'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/terror',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Terror >>'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/thriller',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Thriller >>'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'GetTitles', 'section': 'ALL', 'url': BASE_URL + '/category/western',
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  'Western >>'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'GetSearchQuery'},  {'title':  '[COLOR green]Search[/COLOR]'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'ResolverSettings'}, {'title':  '[COLOR red]Resolver Settings[/COLOR]'}, img=IconPath + '1.png')
+        addon.add_directory({'mode': 'Help'}, {'title':  '[COLOR pink]FOR HELP ON THIS ADDON PLEASE GOTO...[/COLOR] [COLOR gold][B][I]www.xbmchub.com[/B][/I][/COLOR]'}, img=IconPath + '1.png')
+        dialog = xbmcgui.Dialog()
+        dialog.ok(" [COLOR gold] THIS IS A PRE RELEASE [/COLOR]", "[COLOR red]PLEASE DO NOT REPORT BUGS[/COLOR]")
+        xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+
+
+
+def GetSearchQuery():
+	last_search = addon.load_data('search')
+	if not last_search: last_search = ''
+	keyboard = xbmc.Keyboard()
+        keyboard.setHeading('[COLOR green]Search cinevip[/COLOR]')
+	keyboard.setDefault(last_search)
+	keyboard.doModal()
+	if (keyboard.isConfirmed()):
+                query = keyboard.getText()
+                addon.save_data('search',query)
+                Search(query)
+	else:
+                return
+
+        
+def Search(query):
+        url = 'http://www.google.com/search?q=site:cinevip.org ' + query
+        url = url.replace(' ', '+')
+        print url
+        html = net.http_GET(url).content
+        CLEAN(html)
+        match = re.compile('<h3 class="r"><a href="(.+?)".+?onmousedown=".+?">(.+?)</a>').findall(html)
+        for url, title in match:
+                title = title.replace('<b>...</b>', '').replace('<em>', '').replace('</em>', '')
+                addon.add_directory({'mode': 'GetLinks', 'url': url}, {'title':  title})
+	xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+
+if mode == 'main': 
+	MainMenu()
+elif mode == 'GetTitles': 
+	GetTitles(section, url, startPage, numOfPages)
+elif mode == 'GetLinks':
+	GetLinks(section, url)
+elif mode == 'GetSearchQuery':
+	GetSearchQuery()
+elif mode == 'Search':
+	Search(query)
+elif mode == 'PlayVideo':
+	PlayVideo(url, listitem)	
+elif mode == 'ResolverSettings':
+        urlresolver.display_settings()
