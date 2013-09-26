7aebe6e43a4d512a8cdf819c188d93dca64e5fbe
theyid <tcz009@hotmail.co.uk>
2013-09-26 14:46:50 +0100

    still fixing my fuck up

--- plugin.video.scene-down/default.py
+++ plugin.video.scene-down/default.py
@@ -239,7 +239,7 @@ elif mode == 'GetLinks':
 elif mode == 'GetSearchQuery':
 	GetSearchQuery()
 elif mode == 'Search':
-	Search(section, query)
+	Search(query)
 elif mode == 'PlayVideo':
 	PlayVideo(url, listitem)	
 elif mode == 'ResolverSettings':
