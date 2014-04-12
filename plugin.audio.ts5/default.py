0d4b40c0c7881d59d04c8bb3b8fd2f48e4e68c52
TheYid <tcz009@hotmail.co.uk>
2014-04-12 13:37:58 +0100

    update

--- plugin.audio.ts5/default.py
+++ plugin.audio.ts5/default.py
@@ -10,18 +10,10 @@ BASE_URL = 'http://www.ts5.com/'
 net = Net()
 addon = Addon('plugin.audio.ts5', sys.argv)
 
-###### PATHS ###########
-AddonPath = addon.get_path()
-IconPath = AddonPath + "/icons/"
-FanartPath = AddonPath + "/icons/"
-
-##### Queries ##########
 mode = addon.queries['mode']
 url = addon.queries.get('url', None)
 content = addon.queries.get('content', None)
 query = addon.queries.get('query', None)
-startPage = addon.queries.get('startPage', None)
-numOfPages = addon.queries.get('numOfPages', None)
 listitem = addon.queries.get('listitem', None)
 urlList = addon.queries.get('urlList', None)
 section = addon.queries.get('section', None)
@@ -45,11 +37,11 @@ def PlayVideo(url, listitem):
         addon_handle = int(sys.argv[1])
         xbmcplugin.setContent(addon_handle, 'audio')
         li = xbmcgui.ListItem('[COLOR dodgerblue][B]PLAY STREAM[/B][/COLOR]  [COLOR darkorchid][B] >>[/B][/COLOR] >> ', iconImage='https://pbs.twimg.com/media/BTsytR2IAAA5b2g.jpg')
-        li.setProperty('fanart_image', 'http://www.kissfmuk.com/wp-content/uploads/2013/03/craigdavid.jpg')
+        li.setProperty('fanart_image', 'http://i.vimeocdn.com/video/466480811_640.jpg')
         xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
         xbmcplugin.endOfDirectory(addon_handle)
 
-##########################################################################################################################################################################
+################################################################################ ListItem #################################################################################
 
 def GetMediaInfo(html):
         listitem = xbmcgui.ListItem()
@@ -59,22 +51,22 @@ def GetMediaInfo(html):
                 listitem.setInfo('video', {'Title': match.group(1), 'Year': int(match.group(2)) } )
         return listitem
 
-###################################################################### menus ####################################################################################################
+################################################################################# menus ####################################################################################################
 
 def MainMenu():    #homescreen
         addon.add_directory({'mode': 'GetLinks', 'section': 'ALL', 'url': BASE_URL + '/',
-                             'startPage': '1', 'numOfPages': '1'}, {'title':  '[COLOR darkorchid][B]TS5 Music Player[/B] [/COLOR]>>'}, img = 'https://pbs.twimg.com/media/BTsytR2IAAA5b2g.jpg', fanart = 'http://assets2.capitalfm.com/2013/36/craig-david-capital-fm-1378707484-large-article-0.jpg')
+                             'startPage': '1', 'numOfPages': '1'}, {'title':  '[COLOR darkorchid][B]TS5 Music Player[/B] [/COLOR]>>'}, img = 'http://img.youtube.com/vi/MExnrzr7Pa8/0.jpg', fanart = 'http://s29.postimg.org/u0cwxr30n/fanart.jpg')
  
         addon_handle = int(sys.argv[1]) 
         xbmcplugin.setContent(addon_handle, 'audio')
         url = 'http://media-ice.musicradio.com/CapitalXTRALondonMP3.m3u'
-        li = xbmcgui.ListItem('[COLOR darkorchid][B]Capital Xtra[/B][/COLOR] [COLOR red] (((LIVE))) [/COLOR] >>', iconImage='http://www.musicweek.com/cimages/7ec1c3e8cda116edcba97d259933f288.jpg')
-        li.setProperty('fanart_image', 'http://londonist.com/wp-content/uploads/2013/11/Capital-XTRA.jpg')
+        li = xbmcgui.ListItem('[COLOR blue][B]Capital Xtra[/B][/COLOR] [COLOR red] (((LIVE))) [/COLOR] >>', iconImage='http://www.musicweek.com/cimages/a6ff7aa07ec100c4cc84ab4148817d44.jpg')
+        li.setProperty('fanart_image', 'http://s29.postimg.org/u0cwxr30n/fanart.jpg')
         xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
         xbmcplugin.endOfDirectory(addon_handle)
 
 
-#################################################################################################################################################################################
+################################################################################# mode #########################################################################################
 
 if mode == 'main': 
 	MainMenu()
