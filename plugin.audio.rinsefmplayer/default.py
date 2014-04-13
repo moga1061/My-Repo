4ee91e849552c561ef00be1acb93f70b504fde82
TheYid <tcz009@hotmail.co.uk>
2014-04-13 10:18:33 +0100

    new

--- plugin.audio.rinsefmplayer/default.py
+++ plugin.audio.rinsefmplayer/default.py
@@ -3,12 +3,12 @@ import re, string, sys, os
 from t0mm0.common.addon import Addon
 from t0mm0.common.net import Net
 
-addon_id = 'plugin.audio.ts5player'
+addon_id = 'plugin.audio.rinsefmplayer'
 plugin = xbmcaddon.Addon(id=addon_id)
-DB = os.path.join(xbmc.translatePath("special://database"), 'ts5player.db')
-BASE_URL = 'http://www.ts5.com/'
+DB = os.path.join(xbmc.translatePath("special://database"), 'rinsefmplayer.db')
+BASE_URL = 'http://rinse.fm/'
 net = Net()
-addon = Addon('plugin.audio.ts5player', sys.argv)
+addon = Addon('plugin.audio.rinsefmplayer', sys.argv)
 mode = addon.queries['mode']
 url = addon.queries.get('url', None)
 content = addon.queries.get('content', None)
@@ -21,10 +21,10 @@ def GetLinks(url):
         html = net.http_GET(url).content
         listitem = GetMediaInfo(html)
         content = html
-        match = re.compile('data-track-name="(.+?)" data-track-url="(.+?)">').findall(content)
+        match = re.compile('<a href="(.+?)" download="http://podcast.dgen.net/rinsefm/podcast/(.+?)"').findall(content)
         listitem = GetMediaInfo(content)
-        for name, url in match:
-                addon.add_directory({'mode': 'PlayVideo', 'url': url, 'listitem': listitem}, {'title':  name.strip()}, img = 'https://pbs.twimg.com/media/BTsytR2IAAA5b2g.jpg', fanart = 'http://assets2.capitalfm.com/2013/36/craig-david-capital-fm-1378707484-large-article-0.jpg')
+        for url, name in match:
+                addon.add_directory({'mode': 'PlayVideo', 'url': url, 'listitem': listitem}, {'title':  name.strip()}, img = 'http://s16.postimg.org/kdlyi29j9/icon.png', fanart = 'http://s29.postimg.org/rsd7ep7gn/fanart.jpg')
         xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 ############################################################################# Play Video #####################################################################################
@@ -32,8 +32,8 @@ def GetLinks(url):
 def PlayVideo(url, listitem):
         addon_handle = int(sys.argv[1])
         xbmcplugin.setContent(addon_handle, 'audio')
-        li = xbmcgui.ListItem('[COLOR dodgerblue][B]PLAY STREAM[/B][/COLOR]  [COLOR darkorchid][B] >>[/B][/COLOR] >> ', iconImage='https://pbs.twimg.com/media/BTsytR2IAAA5b2g.jpg')
-        li.setProperty('fanart_image', 'http://i.vimeocdn.com/video/466480811_640.jpg')
+        li = xbmcgui.ListItem('[COLOR dodgerblue][B]PLAY STREAM[/B][/COLOR]  [COLOR powderblue][B] >>[/B][/COLOR] >> ', iconImage='http://s16.postimg.org/kdlyi29j9/icon.png')
+        li.setProperty('fanart_image', 'http://s29.postimg.org/rsd7ep7gn/fanart.jpg')
         xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
         xbmcplugin.endOfDirectory(addon_handle)
 
@@ -49,18 +49,17 @@ def GetMediaInfo(html):
 
 ################################################################################# menus ####################################################################################################
 
-def MainMenu():    #homescreen
-        addon.add_directory({'mode': 'GetLinks', '': '', 'url': BASE_URL + '/',
-                             '': '', '': ''}, {'title':  '[COLOR darkorchid][B]TS5 Music Player[/B] [/COLOR]>>'}, img = 'http://img.youtube.com/vi/MExnrzr7Pa8/0.jpg', fanart = 'http://s29.postimg.org/u0cwxr30n/fanart.jpg')
- 
+def MainMenu():   
         addon_handle = int(sys.argv[1]) 
         xbmcplugin.setContent(addon_handle, 'audio')
-        url = 'http://media-ice.musicradio.com/CapitalXTRALondonMP3.m3u'
-        li = xbmcgui.ListItem('[COLOR blue][B]Capital Xtra[/B][/COLOR] [COLOR red] (((LIVE))) [/COLOR] >>', iconImage='http://www.musicweek.com/cimages/a6ff7aa07ec100c4cc84ab4148817d44.jpg')
-        li.setProperty('fanart_image', 'http://s29.postimg.org/u0cwxr30n/fanart.jpg')
+        url = 'http://podcast.dgen.net/rinsefm'
+        li = xbmcgui.ListItem('[COLOR powderblue][B]Rinse FM[/B][/COLOR] [COLOR red] (((LIVE))) [/COLOR] >>', iconImage='http://s16.postimg.org/kdlyi29j9/icon.png')
+        li.setProperty('fanart_image', 'http://s29.postimg.org/rsd7ep7gn/fanart.jpg')
         xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
-        xbmcplugin.endOfDirectory(addon_handle)
 
+        addon.add_directory({'mode': 'GetLinks', '': '', 'url': BASE_URL + '/podcasts/',
+                             '': '', '': ''}, {'title':  '[COLOR dodgerblue][B]Latest shows (Music Player)[/B] [/COLOR]>>'}, img = 'http://s16.postimg.org/kdlyi29j9/icon.png', fanart = 'http://s29.postimg.org/rsd7ep7gn/fanart.jpg')
+        xbmcplugin.endOfDirectory(addon_handle)
 
 ################################################################################# mode #########################################################################################
 
