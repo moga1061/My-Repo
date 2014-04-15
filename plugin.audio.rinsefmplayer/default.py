48e05adfcba3a52ddd65871ef2fe25e2620a6c6d
TheYid <tcz009@hotmail.co.uk>
2014-04-15 20:11:23 +0100

    update

--- plugin.audio.rinsefmplayer/default.py
+++ plugin.audio.rinsefmplayer/default.py
@@ -1,7 +1,13 @@
+# Rinse fm XBMC Plugin
+# Developer: @TheYid009
+# Support: www.xbmchub.com or @TheYid009
+# Disclaimer: @TheYid009 dose not own or publish the content delivered by the plugin
+# streams and content is owned by Rinse fm
+
 import xbmc, xbmcgui, xbmcaddon, xbmcplugin
 import re, string, sys, os
-from t0mm0.common.addon import Addon
-from t0mm0.common.net import Net
+from TheYid.common.addon import Addon
+from TheYid.common.net import Net
 
 addon_id = 'plugin.audio.rinsefmplayer'
 plugin = xbmcaddon.Addon(id=addon_id)
@@ -21,10 +27,21 @@ def GetLinks(url):
         html = net.http_GET(url).content
         listitem = GetMediaInfo(html)
         content = html
+        match = re.compile('<option value="(.+?)">(.+?)</option>').findall(content)
+        listitem = GetMediaInfo(content)
+        for url, name in match:
+                addon.add_directory({'mode': 'GetLinks2', 'url': url, 'listitem': listitem}, {'title':  name.strip()}, img = 'http://2.bp.blogspot.com/-0enPTd9lXMY/TePPr-IU1xI/AAAAAAAABoo/ZL8QxQSk96w/s400/RINSE.png', fanart = 'http://crackmagazine.net/wp-content/uploads/2013/01/rinse_fm_wallpaper_by_peants-d4cinr4.png')
+        xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
+def GetLinks2(url):
+        print 'GETLINKS FROM URL: '+url
+        html = net.http_GET(url).content
+        listitem = GetMediaInfo(html)
+        content = html
         match = re.compile('<a href="(.+?)" download="http://podcast.dgen.net/rinsefm/podcast/(.+?)"').findall(content)
         listitem = GetMediaInfo(content)
         for url, name in match:
-                addon.add_directory({'mode': 'PlayVideo', 'url': url, 'listitem': listitem}, {'title':  name.strip()}, img = 'http://s16.postimg.org/kdlyi29j9/icon.png', fanart = 'http://s29.postimg.org/rsd7ep7gn/fanart.jpg')
+                addon.add_directory({'mode': 'PlayVideo', 'url': url, 'listitem': listitem}, {'title':  name.strip()}, img = 'http://2.bp.blogspot.com/-0enPTd9lXMY/TePPr-IU1xI/AAAAAAAABoo/ZL8QxQSk96w/s400/RINSE.png', fanart = 'http://welikemusic.info/wp-content/uploads/2013/06/amit-dj-krust-rinse-fm-podcast-13-02-2013.jpg')
         xbmcplugin.endOfDirectory(int(sys.argv[1]))
 
 ############################################################################# Play Video #####################################################################################
@@ -32,8 +49,8 @@ def GetLinks(url):
 def PlayVideo(url, listitem):
         addon_handle = int(sys.argv[1])
         xbmcplugin.setContent(addon_handle, 'audio')
-        li = xbmcgui.ListItem('[COLOR dodgerblue][B]PLAY STREAM[/B][/COLOR]  [COLOR powderblue][B] >>[/B][/COLOR] >> ', iconImage='http://s16.postimg.org/kdlyi29j9/icon.png')
-        li.setProperty('fanart_image', 'http://s29.postimg.org/rsd7ep7gn/fanart.jpg')
+        li = xbmcgui.ListItem('[COLOR dodgerblue][B]PLAY STREAM[/B][/COLOR]  [COLOR powderblue][B] >>[/B][/COLOR] >> ', iconImage='http://2.bp.blogspot.com/-0enPTd9lXMY/TePPr-IU1xI/AAAAAAAABoo/ZL8QxQSk96w/s400/RINSE.png')
+        li.setProperty('fanart_image', 'http://fc00.deviantart.net/fs71/f/2013/091/6/7/my_forest_of_contrast_topped_of_by_a_rinse_fm_logo_by_cyrax_apex-d602ggy.jpg')
         xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
         xbmcplugin.endOfDirectory(addon_handle)
 
@@ -53,33 +70,10 @@ def MainMenu():
         addon_handle = int(sys.argv[1]) 
         xbmcplugin.setContent(addon_handle, 'audio')
         url = 'http://podcast.dgen.net/rinsefm'
-        li = xbmcgui.ListItem('[COLOR powderblue][B]Rinse FM[/B][/COLOR] [COLOR red] (((LIVE))) [/COLOR] >>', iconImage='http://s16.postimg.org/kdlyi29j9/icon.png')
+        li = xbmcgui.ListItem('[COLOR powderblue][B]Rinse FM[/B][/COLOR] [COLOR lime] (((LIVE))) [/COLOR] >>', iconImage='http://s16.postimg.org/kdlyi29j9/icon.png')
         li.setProperty('fanart_image', 'http://s29.postimg.org/rsd7ep7gn/fanart.jpg')
         xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
-
-        addon.add_directory({'mode': 'GetLinks', '': '', 'url': BASE_URL + '/podcasts/',
-                             '': '', '': ''}, {'title':  '[COLOR dodgerblue][B]Latest shows (Music Player)[/B] [/COLOR]>>'}, img = 'http://s16.postimg.org/kdlyi29j9/icon.png', fanart = 'http://s29.postimg.org/rsd7ep7gn/fanart.jpg')
-
-        addon.add_directory({'mode': 'GetLinks', '': '', 'url': BASE_URL + '/podcasts/?showID=265',
-                             '': '', '': ''}, {'title':  '[COLOR cadetblue][B]Uncle Dugs (shows)[/B] [/COLOR]>>'}, img = 'http://rinse.fm/wp-content/uploads/2012/09/artistprofile_uncledugs.jpg', fanart = 'http://s29.postimg.org/rsd7ep7gn/fanart.jpg')
-        addon.add_directory({'mode': 'GetLinks', '': '', 'url': BASE_URL + '/podcasts/?showID=187',
-                             '': '', '': ''}, {'title':  '[COLOR cadetblue][B]Dappa (shows)[/B] [/COLOR]>>'}, img = 'http://rinse.fm/wp-content/uploads/2012/09/artistprofile_dappa2.jpg', fanart = 'http://s29.postimg.org/rsd7ep7gn/fanart.jpg')
-        addon.add_directory({'mode': 'GetLinks', '': '', 'url': BASE_URL + '/podcasts/?showID=11361',
-                             '': '', '': ''}, {'title':  '[COLOR cadetblue][B]Flight (shows)[/B] [/COLOR]>>'}, img = 'https://pbs.twimg.com/profile_images/378800000033780049/8585048820bc918b88594175e528d057.jpeg', fanart = 'http://s29.postimg.org/rsd7ep7gn/fanart.jpg')
-        addon.add_directory({'mode': 'GetLinks', '': '', 'url': BASE_URL + '/podcasts/?showID=249',
-                             '': '', '': ''}, {'title':  '[COLOR cadetblue][B]Marcus Nasty (shows)[/B] [/COLOR]>>'}, img = 'http://rinse.fm/wp-content/uploads/2012/09/artistprofile_marcusnasty3.jpg', fanart = 'http://s29.postimg.org/rsd7ep7gn/fanart.jpg')
-        addon.add_directory({'mode': 'GetLinks', '': '', 'url': BASE_URL + '/podcasts/?showID=5661',
-                             '': '', '': ''}, {'title':  '[COLOR cadetblue][B]Metalheadz (shows)[/B] [/COLOR]>>'}, img = 'http://rinse.fm/wp-content/uploads/2013/04/artistprofile_metalheadz_1-460x508.jpg', fanart = 'http://s29.postimg.org/rsd7ep7gn/fanart.jpg')
-        addon.add_directory({'mode': 'GetLinks', '': '', 'url': BASE_URL + '/podcasts/?showID=272',
-                             '': '', '': ''}, {'title':  '[COLOR cadetblue][B]Mark Radford (shows)[/B] [/COLOR]>>'}, img = 'http://rinse.fm/wp-content/uploads/2012/09/artistprofile_markradford1.jpg', fanart = 'http://s29.postimg.org/rsd7ep7gn/fanart.jpg')
-        addon.add_directory({'mode': 'GetLinks', '': '', 'url': BASE_URL + '/podcasts/?showID=224',
-                             '': '', '': ''}, {'title':  '[COLOR cadetblue][B]Sam Supplier (shows)[/B] [/COLOR]>>'}, img = 'http://rinse.fm/wp-content/uploads/2012/09/artistprofile_supplier2.jpg', fanart = 'http://s29.postimg.org/rsd7ep7gn/fanart.jpg')
-        addon.add_directory({'mode': 'GetLinks', '': '', 'url': BASE_URL + '/podcasts/?showID=166',
-                             '': '', '': ''}, {'title':  '[COLOR cadetblue][B]Zinc (shows)[/B] [/COLOR]>>'}, img = 'http://rinse.fm/wp-content/uploads/2012/09/artistprofile_zincv3-460x508.jpg', fanart = 'http://s29.postimg.org/rsd7ep7gn/fanart.jpg')
-        addon.add_directory({'mode': 'GetLinks', '': '', 'url': BASE_URL + '/podcasts/?showID=2853',
-                             '': '', '': ''}, {'title':  '[COLOR cadetblue][B]The Garage Hour (shows)[/B] [/COLOR]>>'}, img = 'http://s16.postimg.org/kdlyi29j9/icon.png', fanart = 'http://s29.postimg.org/rsd7ep7gn/fanart.jpg')
-        addon.add_directory({'mode': 'GetLinks', '': '', 'url': BASE_URL + '/podcasts/?showID=7592',
-                             '': '', '': ''}, {'title':  '[COLOR cadetblue][B]The Grime Show (shows)[/B] [/COLOR]>>'}, img = 'http://s16.postimg.org/kdlyi29j9/icon.png', fanart = 'http://s29.postimg.org/rsd7ep7gn/fanart.jpg')
+        addon.add_directory({'mode': 'GetLinks', 'url': BASE_URL + '/podcasts/'}, {'title':  '[COLOR powderblue][B]Past Shows & Podcasts[/B][/COLOR]'}, img = 'http://s16.postimg.org/kdlyi29j9/icon.png', fanart = 'http://s29.postimg.org/rsd7ep7gn/fanart.jpg')
         xbmcplugin.endOfDirectory(addon_handle)
 
 ################################################################################# mode #########################################################################################
@@ -88,5 +82,7 @@ if mode == 'main':
 	MainMenu()
 elif mode == 'GetLinks':
 	GetLinks(url)
+elif mode == 'GetLinks2':
+	GetLinks2(url)
 elif mode == 'PlayVideo':
 	PlayVideo(url, listitem)	
